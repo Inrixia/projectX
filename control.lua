3d9bcbbb0d826b28f6610be330871ba06e6b5436
Inrixia <Inrixia@gmail.com>
2023-05-25 10:05:22 +1200

    Working stack limits & overflows

--- control.lua
+++ control.lua
@@ -3,12 +3,12 @@ ExchangeRate = {
     ["iron-plate"] = 1,
     ["copper-plate"] = 2,
 }
-EMC = 200;
+EMC = 100;
 EMCOverflow = 0;
 PreviousStorage = {};
 
 function PopulateStorage()
-    if StorageInventory == nil or EMC == 0 then
+    if StorageInventory == nil then
         return
     end
 
@@ -16,22 +16,33 @@ function PopulateStorage()
     PreviousStorage = {}
     EMCOverflow = 0;
 
+    if EMC == 0 then
+        return
+    end
+
     for name, rate in pairs(ExchangeRate) do
-        local count = EMC / rate;
-        EMCOverflow = EMCOverflow + EMC % rate;
-        if (count <= 0) then
-            StorageInventory.remove{name = name}
-        else
+        local count = math.floor(EMC / rate);
+        local item_prototype = game.item_prototypes[name]
+        local max_stack_size = item_prototype.stack_size
+
+        if (count >= 1) then
+            -- Limit count to the max stack size
+            count = math.min(count, max_stack_size)
+
+            EMCOverflow = EMCOverflow + EMC % rate;
+
             StorageInventory.insert{name = name, count = count}
             PreviousStorage[name] = count;
         end
     end
+    
 end
 
 function OnCreate(event)
     local entity = event.created_entity
     if entity.name == "exchange-entity" then
         StorageInventory = entity.get_inventory(defines.inventory.chest)
+        StorageInventory.clear()
     end
 end
 
@@ -43,16 +54,25 @@ function OnDestroy(event)
 end
 
 function OnTick(event)
-    -- if event.tick % 120 ~= 0 then
-    --     return
-    -- end
     if StorageInventory ~= nil then
         local emcChange = 0;
-        for name, count in pairs(StorageInventory.get_contents()) do
-            emcChange = emcChange + (count - (PreviousStorage[name] or 0)) * (ExchangeRate[name] or 0)
+
+        -- Store the contents of StorageInventory
+        local storageContents = StorageInventory.get_contents()
+
+        -- Create a table with unique keys from both storageContents and PreviousStorage
+        local combinedKeys = {}
+        for name in pairs(storageContents) do combinedKeys[name] = true end
+        for name in pairs(PreviousStorage) do combinedKeys[name] = true end
+
+        -- Iterate over combinedKeys
+        for name in pairs(combinedKeys) do
+            local newCount = storageContents[name] or 0
+            local oldCount = PreviousStorage[name] or 0
+            emcChange = emcChange + (newCount - oldCount) * (ExchangeRate[name] or 0)
         end
 
-        EMC = EMC + emcChange + EMCOverflow;
+        EMC = math.max(0, EMC + emcChange + EMCOverflow);
 
         PopulateStorage()
     end
