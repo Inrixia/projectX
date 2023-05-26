0684b65b9da1b7bff1444da7604358110fb7b102
Inrixia <Inrixia@gmail.com>
2023-05-26 22:28:00 +1200

    Working energy consumption

--- prototypes/Requester.lua
+++ prototypes/Requester.lua
@@ -4,7 +4,7 @@ local getMissingRequests = require("control/lib/getMissingRequests")
 -- Class
 local Requester = {
 	name = "projectx-requester",
-	itemName = "ProjectX Requester",
+	itemName = "projectx-requester-item",
 }
 
 function Requester:BuildProto()
@@ -48,28 +48,41 @@ end
 
 function Requester:OnTick(event)
 	for unit_number, requesterChest in pairs(global.Requesters) do
-		if (requesterChest.logistic_network ~= nil) then
-			for i, request in ipairs(getMissingRequests(requesterChest)) do
-				while request.count > 0 do
-					local pickupPoint = requesterChest.logistic_network.select_pickup_point{name=request.name}
-					if pickupPoint == nil then
-						break
-					end
-					-- local distance = calculateEntityDistance(pickupPoint.owner.position, requesterChest.position)
-					local pickupInventory = pickupPoint.owner.get_inventory(defines.inventory.chest)
-					local requesterInventory = requesterChest.get_inventory(defines.inventory.chest)
-					
-					local pickupInventoryAvalible = pickupInventory.get_item_count(request.name)
 
-					local transferAmount = math.min(pickupInventoryAvalible, request.count)
-					if transferAmount > 0 then
-						pickupInventory.remove{name = request.name, count = transferAmount}
-						requesterInventory.insert{name = request.name, count = transferAmount}
-						request.count = request.count - transferAmount
-					end
+		if requesterChest.logistic_network == nil then goto continue end
+
+		local nearestCell = requesterChest.logistic_network.find_cell_closest_to(requesterChest.position)
+		if (nearestCell.owner == nil) then goto continue end
+		if (nearestCell.owner.energy < 100000) then goto continue end
+
+		for i, request in pairs(getMissingRequests(requesterChest)) do
+			while request.count > 0 do
+				local pickupPoint = requesterChest.logistic_network.select_pickup_point{name=request.name}
+				if pickupPoint == nil then
+					break
+				end
+				local pickupInventory = pickupPoint.owner.get_inventory(defines.inventory.chest)
+				local requesterInventory = requesterChest.get_inventory(defines.inventory.chest)
+
+				local pickupInventoryAvalible = pickupInventory.get_item_count(request.name)
+
+				local maxTransferAmount = 1;
+
+				local transferAmount = math.min(pickupInventoryAvalible, maxTransferAmount)
+				if transferAmount > 0 then
+					local distance = calculateEntityDistance(pickupPoint.owner.position, requesterChest.position)
+					local requiredPower = transferAmount*distance*1000
+
+					if nearestCell.owner.energy < requiredPower then goto continue end
+					nearestCell.owner.energy = nearestCell.owner.energy - requiredPower
+
+					pickupInventory.remove{name = request.name, count = transferAmount}
+					requesterInventory.insert{name = request.name, count = transferAmount}
+					request.count = maxTransferAmount - transferAmount
 				end
 			end
 		end
+		::continue::
 	end
 end
 
