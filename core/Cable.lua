4df05bfa3b0e83a63aaaec0fda6612e6bac290c1
Inrixia <Inrixia@gmail.com>
2024-01-14 18:37:41 +1300

    Working cable temp

--- core/Cable.lua
+++ core/Cable.lua
@@ -1,3 +1,12 @@
 local NetworkedEntity = require("_NetworkedEntity")
 
 NetworkedEntity.new(require("proto/Cable"))
+	:onChannels(function(netEnt)
+		netEnt.entity.temperature = 1
+	end)
+	:onNoChannels(function(netEnt)
+		netEnt.entity.temperature = 0
+	end)
+	:onJoinedNetwork(function(netEnt)
+		netEnt.entity.temperature = netEnt.network.channels
+	end)
--- core/proto/Cable.lua
+++ core/proto/Cable.lua
@@ -4,9 +4,9 @@ return ProtoBase.new("projectX_cable", function(prototypeName)
 	local entity = table.deepcopy(data.raw["heat-pipe"]["heat-pipe"])
 	entity.name = prototypeName
 	entity.heat_buffer.connections = {}
-	entity.heat_buffer.max_temperature = 8
+	entity.heat_buffer.max_temperature = 1
 	entity.heat_buffer.min_working_temperature = 0
-	entity.heat_buffer.minimum_glow_temperature = 1
+	entity.heat_buffer.minimum_glow_temperature = 0
 	entity.heat_buffer.default_temperature = 0
 	-- Item
 	local item = table.deepcopy(data.raw.item["iron-chest"])
